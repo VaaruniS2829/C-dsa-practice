58266eeb828d212103b769d0625a42f77551a130
Vaaruni S <vaarunis20@gmail.com>
2025-08-16 23:38:04 +0530

    01_linked_list.c

---
+++ 01_linked_list.c
@@ -0,0 +1,34 @@
+// Singly Linked List Implementation //
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+int main(){
+    struct node *head=0, *newnode=0 , *temp;
+    int choice;
+    while(choice){
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data ");
+        scanf("%d" , &newnode->data);
+        newnode->next=0;
+        if(head==0){
+            head=temp=newnode;
+        }
+        else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+        printf("Do you want to continue(0 for no,1 for yes)?");
+        scanf("%d" , &choice);
+        }
+        temp=head;
+        printf("Linked List:\n");
+        while(temp!=0){
+            printf("%d\n" , temp->data);
+            temp=temp->next;
+        }
+    return 0;
+}
