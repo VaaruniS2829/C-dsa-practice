176e965eb996b8ed41cc00ad163f1e7a3d212556
Vaaruni S <vaarunis20@gmail.com>
2025-09-11 23:33:06 +0530

    13_Doubly_linked_list.c

---
+++ 13_Doubly_linked_list.c
@@ -0,0 +1,36 @@
+Doubly Linked List Implementation:
+
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+struct node *head=0 , *newnode=0 , *temp;
+int main(){
+    int choice;
+    while(choice){
+        newnode =(struct node*)malloc(sizeof(struct node));
+        printf("Enter data:\n");
+        scanf("%d" , &newnode->data);
+        newnode->next=0;
+        if(head==0){
+            head=temp=newnode;
+        }
+        else{
+            temp->next=newnode;
+            newnode->prev=temp;
+            temp=newnode;
+        }
+        printf("Do you want to continue (0 for no and 1 for yes)?");
+        scanf("%d" , &choice);
+    }
+    temp=head;
+    printf("Doubly linked list:\n");
+    while(temp!=0){
+        printf("%d\n" , temp->data);
+        temp=temp->next;
+    }
+    return 0;
+}
